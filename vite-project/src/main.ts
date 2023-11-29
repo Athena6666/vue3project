acb989ef21e24eb0a580b1c57c28c8cd9880e52d
Athena6666 <88647638+Athena6666@users.noreply.github.com>
2023-11-29 18:04:41 +0800

    chore: update

--- vite-project/src/main.ts
+++ vite-project/src/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 //引入element-plus插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
@@ -9,7 +9,7 @@ import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 const app = createApp(App)
 // 安装element-plus插件
 app.use(ElementPlus,{
-  locale: zhCn
+  locale: zhCn//element-plus国际化配置
 })
 // 将应用挂载到挂载点上
 app.mount('#app')
